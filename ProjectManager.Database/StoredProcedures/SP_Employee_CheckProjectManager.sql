6c696e4e195c50eb62077cf980c0fc5bae39bd8a
Samuel Legrain <samuel.legrain@bstorm.be>
2026-03-25 14:38:15 +0100

    Ajoutez des fichiers projet.

---
+++ ProjectManager.Database/StoredProcedures/SP_Employee_CheckProjectManager.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[SP_Employee_CheckProjectManager]
+	@employeeId UNIQUEIDENTIFIER
+AS
+BEGIN
+	SELECT	[IsProjectManager]
+		FROM [Employee]
+		WHERE [EmployeeId] = @employeeId
+END
---
+++ ProjectManager.Database/StoredProcedures/SP_Employee_GetFree.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[SP_Employee_GetFree]
+AS
+BEGIN
+	SELECT	[EmployeeId]
+		FROM	[Employee]
+	EXCEPT
+	SELECT	[EmployeeId]
+		FROM [TakePart]
+		WHERE	GETDATE() > [StartDate] 
+			AND ([EndDate] IS NULL OR [EndDate] > GETDATE())
+END
